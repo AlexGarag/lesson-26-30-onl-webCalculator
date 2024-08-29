590113940e8e0b71a04ddabf2abb0bd65d6c817e
alex garag <alex.garag@bk.ru>
2024-08-29 14:59:57 +0300

    HistoryCalculator выдаёт результат на страницу

--- src/main/java/HistoryCalculator.java
+++ src/main/java/HistoryCalculator.java
@@ -3,6 +3,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import static by.tms.lesson26.onl30.other.Historian.readHistoryCalculation;
 import static by.tms.lesson26.onl30.other.KeeperConstants.*;
@@ -10,12 +15,47 @@ import static by.tms.lesson26.onl30.other.MyLogger.logIn;
 
 @WebServlet("/history_calculator")
 public class HistoryCalculator extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         if (IS_PERFORM_LOGGING) logIn(MESSAGE_BEGINNING_WORK_TEMPLATE.formatted("HistoryCalculator"));
         String[] historyArray = readHistoryCalculation().split("\n");
         int i = 0;
-//        Response response = prepareResponseHistory(historyArray);
-//        exchangeAll(exchange, response)
+        resp.getWriter().write(prepareResponseHistory(historyArray));
         if (IS_PERFORM_LOGGING) logIn(MESSAGE_ENDING_WORK_TEMPLATE.formatted("HistoryCalculator"));
+    }
+
+   private String prepareResponseHistory(String[] historyArray) {
+        StringBuilder resultString =  new StringBuilder();
+        for (String line : historyArray) {
+            String[] lineArray = line.split(";");
+            resultString.append(convertSecondsToStringDateTime(Long.valueOf(lineArray[0]))).append("\t---\t");
+            double firstOperand = Double.valueOf(lineArray[1]);
+            double secondOperand = Double.valueOf(lineArray[2]);
+            switch (lineArray[3]) {
+                case "sum" -> resultString.append(myDoubleString(firstOperand)).append("+").append(myDoubleString(secondOperand)).append("=")
+                        .append(myDoubleString(firstOperand + secondOperand)).append("\n");
+                case "diff" -> resultString.append(myDoubleString(firstOperand)).append("-").append(myDoubleString(secondOperand)).append("=")
+                        .append(myDoubleString(firstOperand - secondOperand)).append("\n");
+                case "mul" -> resultString.append(myDoubleString(firstOperand)).append("*").append(myDoubleString(secondOperand)).append("=")
+                        .append(myDoubleString(firstOperand * secondOperand)).append("\n");
+                case "div" -> resultString.append(myDoubleString(firstOperand)).append("/").append(myDoubleString(secondOperand)).append("=")
+                        .append(myDoubleString(firstOperand / secondOperand)).append("\n");
+                case "prc" -> resultString.append(myDoubleString(firstOperand)).append("%").append(myDoubleString(secondOperand)).append("=")
+                        .append(myDoubleString(firstOperand * secondOperand / 100)).append("\n");
+            };
+        }
+        return resultString.toString();
+    }
+
+    String convertSecondsToStringDateTime(long quantitySeconds){
+        Date dateLong = new Date(quantitySeconds);
+        Instant instant = dateLong.toInstant();
+        ZoneId defaultZoneId = ZoneId.systemDefault();
+        LocalDateTime localDateTime = instant.atZone(defaultZoneId).toLocalDateTime();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_TIME_LOGGER_TEMPLATE);
+        return localDateTime.format(dateTimeFormatter);
+    }
+
+    String myDoubleString(double d){
+        return String.format(DOUBLE_2_STRING,d);
     }
 }
--- src/main/java/by/tms/lesson26/onl30/other/KeeperConstants.java
+++ src/main/java/by/tms/lesson26/onl30/other/KeeperConstants.java
@@ -12,5 +12,6 @@ public class KeeperConstants {
     public static final String CSV_FORMAT_TEMPLATE = "%s" + SEPARATOR + "%s" + SEPARATOR + "%s" + SEPARATOR + "%s\n";
     public static final String LOGGER_TEMPLATE = "%s, %s\n";
     public static final String DATE_TIME_LOGGER_TEMPLATE = "dd/MM/yyyy HH:mm:ss";
+    public static final String DOUBLE_2_STRING = "%.2f";
     public static final boolean IS_PERFORM_LOGGING = true;
 }

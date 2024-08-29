a44aed59bd5cb5f07d7bdc9b84e11d7abebc742d
alex garag <alex.garag@bk.ru>
2024-08-29 12:17:12 +0300

    калькулятор вычисляет и выдаёт результат

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,13 +3,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/calculator")
+import java.io.IOException;
+
+import static by.tms.lesson26.onl30.other.KeeperConstants.*;
+
+@WebServlet("/calculator")      //http://localhost:8080/calculator?num1=7&num2=5&type=sum
 public class Calculator extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (IS_PERFORM_LOGGING) {
+            System.out.println(MESSAGE_BEGINNING_WORK_TEMPLATE.formatted("'Calculator'"));
+        }
+        String operand1 = req.getParameter("num1");
+        String operand2 = req.getParameter("num2");
+        String typeOperation = req.getParameter("type");
+
+        double num1 = Double.valueOf(operand1);
+        double num2 = Double.valueOf(operand2);
+        String result = calculate(num1, num2, typeOperation);
+
+        resp.getWriter().write(RESPONSE_TEMPLATE.formatted(result));
+// todo сделать историю операций
 int i = 0;
-        System.out.println(i);
-        //        resp.getWriter().write(RESPONSE_TEMPLATE.formatted(NAME_CITY, shapeResponse(ZONE_ID, DATE_FORMAT)));
+    }
+
+    private String calculate(double num1, double num2, String typeOperation) {
+        return  switch (typeOperation) {
+            case "sum" -> String.valueOf(num1 + num2);
+            case "diff" -> String.valueOf(num1 - num2);
+            case "mul" -> String.valueOf(num1 * num2);
+            case "div" -> String.valueOf(num1 / num2);
+            case "prc" -> String.valueOf(num1 * num2 / 100);
+            default -> INVALID_OPERATION;
+        };
     }
 }
---
+++ src/main/java/by/tms/lesson26/onl30/other/KeeperConstants.java
@@ -0,0 +1,8 @@
+package by.tms.lesson26.onl30.other;
+
+public class KeeperConstants {
+    public static final String MESSAGE_BEGINNING_WORK_TEMPLATE = "the servlet %s has started working";
+    public static final String INVALID_OPERATION = "invalid operation";
+    public static final String RESPONSE_TEMPLATE = "result operation: %s";
+    public static final boolean IS_PERFORM_LOGGING = true;
+}
